d2ab6b3c8c53ad0b7aa6cef16275b1a962e3919f
offtopshelf <offttop@icloud.com>
2024-08-26 19:09:00 -0500

    Add files via upload

---
+++ Lesson 1 Homework.py
@@ -0,0 +1,15 @@
+#String/Math Assignment
+import math
+
+def main():
+
+    firstName = "Zoro"
+    lastName = "Roronoa"
+    print(lastName + " " + firstName)
+    print(lastName[3])
+    print(lastName[1:5])
+    print(math.factorial(12))
+    
+
+if __name__ == "__main__":
+    main()
